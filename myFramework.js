8fa60ffa236e4c13b3856bba161f33314ef1fc59
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2021-09-10 12:28:02 +0530

    asdf

--- myFramework.js
+++ myFramework.js
@@ -1,14 +1,22 @@
 //      INTERSECTION OBSERVER
-let itemAnime = document.getElementsByClassName("itemAnime");
+let itemAnime = document.querySelectorAll(".itemAnime");
 let observer = new IntersectionObserver(callBack);
 for (var i = 0; i < itemAnime.length; i++) {
     observer.observe(itemAnime[i]);
 }
 function callBack (Entries) {
-    Entries.forEach(entry => {
+    Entries.forEach((entry, index) => {
         if (entry.isIntersecting) {
-            if(entry.target.tagName == 'A') {
-                entry.target.classList.add('fadeLeftLine');
+            if (index / 2 == 0) {
+                if (entry.target.tagName == "P" || entry.target.tagName == "A" || entry.target.tagName == "SPAN") {
+                    entry.target.classList.add("fadeRight");
+                }
+                else {
+                    entry.target.classList.add("fadeRightLine");
+                }
+            }
+            else {
+                entry.target.classList.add("fadeLeftLine");
             }
         }
     })
@@ -20,7 +28,6 @@ function toggleClass (element, className) {
 }
 
 window.addEventListener("scroll", () => {
-    console.log(document.querySelector("nav"));
     document.querySelector("nav").classList.toggle("navbar1", window.scrollY > 0);
 })
 
