19a79981c75de6f46d620a592fae77fe2f262629
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2021-09-10 23:23:35 +0530

    asdfasdfads

--- myFramework.js
+++ myFramework.js
@@ -51,3 +51,17 @@ document.getElementsByClassName("hamburger")[0].addEventListener("click", () => 
     document.getElementsByClassName("modal")[0].classList.toggle("modal1");
 });
 
+// FORM
+var form = document.getElementById('sheetdb-form');
+        form.addEventListener("submit", e => {
+          e.preventDefault();
+          fetch(form.action, {
+              method : "POST",
+              body: new FormData(document.getElementById("sheetdb-form")),
+          }).then(
+              response => response.json()
+          ).then((html) => {
+            // you can put any JS code here
+            alert('We have recieved your message and will contact you soon ❗ ⏳')
+          });
+        });
